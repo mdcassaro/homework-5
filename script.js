a1be5a941ba61b1916e60fbf7e042c73337aed60
Michael Cassaro <mdcassaro@hotmail.com>
2020-03-26 18:56:13 -0500

    added time to front page and linked moment

--- script.js
+++ script.js
@@ -1,79 +1,137 @@
 
-var modalEl = document.querySelector("#modal-container");
-var modalNameEl = document.querySelector("#modal-name");
-var descriptionEl = document.querySelector("#description");
-var closeEl = document.querySelector(".close");
-var saveBtn = document.querySelector("#save");
+// var modalEl = document.querySelector("#modal-container");
+// var modalNameEl = document.querySelector("#modal-name");
+// var descriptionEl = document.querySelector("#description");
+// var closeEl = document.querySelector(".close");
+// var saveBtn = document.querySelector("#save");
 
-var slot9 = $("#9")
-var slot10 = $("#10")
-var slot11 = $("#11")
-var slot12 = $("#12")
-var slot1 = $("#1")
-var slot2 = $("#2")
-var slot3 = $("#3")
-var slot4 = $("#4")
-var slot5 = $("#5")
+var text9 = $("#text9").val().trim();
+var text10 = $("#text10").val().trim();
+var text11 = $("#text11").val().trim();
+var text12 = $("#text12").val().trim();
+var text1 = $("#text1").val().trim();
+var text2 = $("#text2").val().trim();
+var text3 = $("#text3").val().trim();
+var text4 = $("#text4").val().trim();
+var text5 = $("#text5").val().trim();
+
+// var texts = [text9, text10, text11, text12, text1, text2, text3, text4, text5]
+
+var NowMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
+var eDisplayMoment = document.getElementById('currentDay');
+eDisplayMoment.innerHTML = NowMoment;
 
 
 
-var toDo = [slot9, slot10, slot11, slot12, slot1, slot2, slot3, slot4, slot5]
 
 
-//cods for modal container opening
-function handleClick(event) {
-    if (event.target.matches("button")) {
-      //if tbis button is clicked
-      event.preventDefault();
-      //prevents the name from being removed
-      modalEl.style.display = "block";
-      currentId = parseInt(event.target.parentElement.id);
-      var name = todo.name;
-      var description = toDo.description;
-      modalNameEl.textContent = name;
-      if(description) {
-        descriptionEl.value = description;
-      } else {
-        descriptionEl.value = "";
-      }
+
+// var toDo = [slot9, slot10, slot11, slot12, slot1, slot2, slot3, slot4, slot5]
+
+
+
+
+
+// var modalEl = $("#modal-container");
+// var modalNameEl = $("#modal-name");
+// var descriptionEl = $("#description");
+// var closeEl = $(".close");
+// var saveBtn = $("#save");
+// var nameEl = $("#name");
+
+// var slot9 = $("#9")
+// var slot10 = $("#10")
+// var slot11 = $("#11")
+// var slot12 = $("#12")
+// var slot1 = $("#1")
+// var slot2 = $("#2")
+// var slot3 = $("#3")
+// var slot4 = $("#4")
+// var slot5 = $("#5")
+
+
+
+
+
+
+
+function storeTodos9() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos9", JSON.stringify(text9));
+}
+function storeTodos10() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos10", JSON.stringify(text10));
+}
+function storeTodos11() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos11", JSON.stringify(text11));
+}
+function storeTodos12() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos12", JSON.stringify(text12));
+}
+function storeTodos1() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos1", JSON.stringify(text1));
+}
+function storeTodos2() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos2", JSON.stringify(text2));
+}
+function storeTodos3() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos3", JSON.stringify(text3));
+}
+function storeTodos4() {
+    // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos4", JSON.stringify(text4));
+}
+function storeTodos5() {
+        // Stringify and set "todos" key in localStorage to todos array
+    localStorage.setItem("todos5", JSON.stringify(text5));
+}
+
+function init() {
+    alert("2nd button works")
+    
+    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+    
+  
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (storedTodos !== null) {
+      texts = storedTodos;
     }
 }
-function close() {
-    modalEl.style.display = "none";
-}
+$("button").on("click", function(event){
+    event.preventDefault();
+
+    alert("button worked");
+
+    storeTodos9();
+    storeTodos10();
+    storeTodos11();
+    storeTodos12();
+    storeTodos1();
+    storeTodos2();
+    storeTodos3();
+    storeTodos4();
+    storeTodos5();
+
 
   
-//codes save button
-closeEl.addEventListener("click", close);
-  saveBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    people[currentId].description = descriptionEl.value;
-    close();
-});
+})
+
+
+  
+    // Render todos to the DOM
 
 
 
 
 
+// renderTodos();
 
 
 
 
-
-
-// localStorage.setItem("slot8", value)
-// localStorage.setItem("slot9", value)
-// localStorage.setItem("slot10", value)
-// localStorage.setItem("slot11", value)
-// localStorage.setItem("slot12", value)
-// localStorage.setItem("slot1", value)
-// localStorage.setItem("slot2", value)
-// localStorage.setItem("slot3", value)
-// localStorage.setItem("slot4", value)
-// localStorage.setItem("slot5", value)
-
-
-// var slots = [1,2,3];
-// localStorage.setItem("slots", JSON.stringify(slots));
-// slots = JSOn.parse(localStorage.getItem("slots"));
-// //make the divs have an id and var the id to the slot
