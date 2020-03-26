0571bf7a6430e804d13fd2e4f17e30365d9af6fc
Michael Cassaro <mdcassaro@hotmail.com>
2020-03-26 15:44:47 -0500

    time buttons work for console log

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ var descriptionEl = document.querySelector("#description");
 var closeEl = document.querySelector(".close");
 var saveBtn = document.querySelector("#save");
 
+var slot9 = $("#9")
+var slot10 = $("#10")
+var slot11 = $("#11")
+var slot12 = $("#12")
+var slot1 = $("#1")
+var slot2 = $("#2")
+var slot3 = $("#3")
+var slot4 = $("#4")
+var slot5 = $("#5")
+
+
+
+var toDo = [slot9, slot10, slot11, slot12, slot1, slot2, slot3, slot4, slot5]
+
+
 //cods for modal container opening
 function handleClick(event) {
     if (event.target.matches("button")) {
@@ -13,8 +28,8 @@ function handleClick(event) {
       //prevents the name from being removed
       modalEl.style.display = "block";
       currentId = parseInt(event.target.parentElement.id);
-      var name = people[currentId].name;
-      var description = people[currentId].description;
+      var name = todo.name;
+      var description = toDo.description;
       modalNameEl.textContent = name;
       if(description) {
         descriptionEl.value = description;
@@ -22,7 +37,19 @@ function handleClick(event) {
         descriptionEl.value = "";
       }
     }
-  }
+}
+function close() {
+    modalEl.style.display = "none";
+}
+
+  
+//codes save button
+closeEl.addEventListener("click", close);
+  saveBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    people[currentId].description = descriptionEl.value;
+    close();
+});
 
 
 
@@ -34,19 +61,19 @@ function handleClick(event) {
 
 
 
-localStorage.setItem("slot8", value)
-localStorage.setItem("slot9", value)
-localStorage.setItem("slot10", value)
-localStorage.setItem("slot11", value)
-localStorage.setItem("slot12", value)
-localStorage.setItem("slot1", value)
-localStorage.setItem("slot2", value)
-localStorage.setItem("slot3", value)
-localStorage.setItem("slot4", value)
-localStorage.setItem("slot5", value)
+// localStorage.setItem("slot8", value)
+// localStorage.setItem("slot9", value)
+// localStorage.setItem("slot10", value)
+// localStorage.setItem("slot11", value)
+// localStorage.setItem("slot12", value)
+// localStorage.setItem("slot1", value)
+// localStorage.setItem("slot2", value)
+// localStorage.setItem("slot3", value)
+// localStorage.setItem("slot4", value)
+// localStorage.setItem("slot5", value)
 
 
-var slots = [1,2,3];
-localStorage.setItem("slots", JSON.stringify(slots));
-slots = JSOn.parse(localStorage.getItem("slots"));
-//make the divs have an id and var the id to the slot
+// var slots = [1,2,3];
+// localStorage.setItem("slots", JSON.stringify(slots));
+// slots = JSOn.parse(localStorage.getItem("slots"));
+// //make the divs have an id and var the id to the slot
